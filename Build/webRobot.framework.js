993f0f0c52e3e91d67748ca751ceba504b9ca07e
unknown <>
2021-01-29 19:50:21 -0500

    fixed pictures?

--- Build/webRobot.framework.js
+++ Build/webRobot.framework.js
@@ -1297,7 +1297,7 @@ function _emscripten_asm_const_ii(code, a0) {
  return ASM_CONSTS[code](a0);
 }
 STATIC_BASE = GLOBAL_BASE;
-STATICTOP = STATIC_BASE + 2645392;
+STATICTOP = STATIC_BASE + 2643968;
 __ATINIT__.push({
  func: (function() {
   __GLOBAL__sub_I_AccessibilityScriptingClasses_cpp();
@@ -3235,7 +3235,7 @@ __ATINIT__.push({
   ___emscripten_environ_constructor();
  })
 });
-var STATIC_BUMP = 2645392;
+var STATIC_BUMP = 2643968;
 Module["STATIC_BASE"] = STATIC_BASE;
 Module["STATIC_BUMP"] = STATIC_BUMP;
 var tempDoublePtr = STATICTOP;
@@ -15512,8 +15512,8 @@ function nullFunc_vjji(x) {
  err("Build with ASSERTIONS=2 for more info.");
  abort(x);
 }
-Module["wasmTableSize"] = 56007;
-Module["wasmMaxTableSize"] = 56007;
+Module["wasmTableSize"] = 56005;
+Module["wasmMaxTableSize"] = 56005;
 function invoke_dddi(index, a1, a2, a3) {
  var sp = stackSave();
  try {
